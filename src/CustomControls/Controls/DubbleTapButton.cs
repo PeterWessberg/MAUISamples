a75dacd342829ff64364498743fb8d176709c585
Peter Wessberg <peter@wessberg.com>
2023-10-25 23:39:41 +0200

    Double Tap Button IPhone adjustment

--- src/CustomControls/Controls/DubbleTapButton.cs
+++ src/CustomControls/Controls/DubbleTapButton.cs
@@ -8,7 +8,7 @@ namespace CustomControls.Controls
         private bool _canExecutCommand = false;
         private ICommand _toggleCommand;
         private string _fontFamily;
-        private int width = 35;
+        private int width = 30;
 
         public new event EventHandler Clicked;
 
@@ -22,7 +22,7 @@ namespace CustomControls.Controls
                         new Animation(v => WidthRequest = v, width, 70).Commit(this, "Animate", 16, 200, Easing.SinOut, finished: (v, c) =>
                         {
                             FontFamily = _fontFamily;
-                            TextColor = Colors.Black;
+                            TextColor = Colors.DarkGray;
                             FontSize = 14;
                             CornerRadius = 18;
                             _canExecutCommand = true;
@@ -56,7 +56,7 @@ namespace CustomControls.Controls
             Text = FontAwesomeIcons.Xmark;
             Padding = 0;
             FontSize = 16;
-            TextColor = Colors.Black;
+            TextColor = Colors.DarkGray;
             WidthRequest = width;
             HeightRequest = width;
             BackgroundColor = Colors.LightGray;
@@ -67,7 +67,7 @@ namespace CustomControls.Controls
         {
             TextColor = Colors.Transparent;
             Text = FontAwesomeIcons.Xmark;
-            new Animation(v => WidthRequest = v, 70, width).Commit(this, "Animate", 16, 200, Easing.SinOut, finished: (v, c) =>
+            new Animation(v => WidthRequest = v, 70, width).Commit(this, "AnimateReset", 16, 200, Easing.SinOut, finished: (v, c) =>
             {
                 FontFamily = "FontAwesome-Solid";
                 TextColor = Colors.Black;
